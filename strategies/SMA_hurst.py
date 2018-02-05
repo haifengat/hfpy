862e712ae66e3a39c639b5886f7e3a3194cec162
anoval <3202923@qq.com>
2018-02-05 18:49:59 +0800

    test

--- strategies/SMA_hurst.py
+++ strategies/SMA_hurst.py
@@ -22,11 +22,11 @@ class SMA_hurst(Strategy):
         self.p_ma1 = self.Params['MA1'] 
         self.p_ma2 = self.Params['MA2'] 
         self.p_lots = self.Params['Lots'] = 1
-        self.hurst = []
+        self.hurstlist = []
         self.mahurstlist = []
         self.vola = []
     #github版本 hurst 指数
-    def hurst(self,ts):
+    def churst(self,ts):
         """Returns the Hurst Exponent of the time series vector ts"""
 
         # create the range of lag values
@@ -41,7 +41,7 @@ class SMA_hurst(Strategy):
         # Return the Hurst Exponent from the polyfit output
         return poly[0] * 2.0
 
-    # 自定义版本 hurst指数
+    # 自定义版本 hurst指数计算方法
     def calcHurst2(self, ts):
         '''
         if not isinstance(ts, Iterable):
@@ -68,7 +68,8 @@ class SMA_hurst(Strategy):
     def OnBarUpdate(self, data=Data, bar=Bar):
         if len(self.C) > 2:
             volability = np.log(self.C[-1]) - np.log(self.C[-2])
-            self.vola.append(volability)
+            if self.D[-1] != self.D[-2]:
+                self.vola.append(volability)
 
         if len(self.C) < self.p_ma2:
             return
@@ -76,44 +77,43 @@ class SMA_hurst(Strategy):
         if len(self.C) < 50:
             return
 
-
-
         # print('{0}-{1}'.format(self.D[-1], self.C[-1]))
         ma1 = talib.SMA(self.C, self.p_ma1)
         ma2 = talib.SMA(self.C, self.p_ma2)
 
         # 实盘需要考虑tick的连续添加
-        self.hurst.append(np.array(self.calcHurst2(self.vola[-30:-1])))   
+        #self.hurstlist.append(np.array(self.churst(self.vola[-30:-1])))   
+        
+        self.hurstlist.append(np.array(self.calcHurst2(self.vola[-30:-1])))   
 
 
-        if len(self.hurst) > 5:
-            mahurst = np.mean(self.hurst[-6:-2]) 
+        if len(self.hurstlist) > 5:
+            mahurst = np.mean(self.hurstlist[-6:-2]) 
             self.mahurstlist.append(mahurst)
             #talib.SMA(self.hurst,5)
+            print('hurst = ',mahurst)
             print('date = ',self.D[-1])
             #print('***********************************')
 
             self.IndexDict['ma5'] = ma1
             self.IndexDict['ma10'] = ma2
 
-
-
             if self.PositionLong == 0:
                 if ma1[-1] >= ma2[-1] and ma1[-2] < ma2[-2]:
                     if self.PositionShort > 0:
                         self.BuyToCover(self.O[-1], self.p_lots, '买平')
-                    if mahurst > 0.6:
+                    if mahurst > 1 and mahurst < 1.25:
                         self.Buy(self.O[-1], self.p_lots, '买开')
             elif self.PositionShort == 0:
                 if ma1[-1] <= ma2[-1] and ma1[-2] > ma2[-2]:
                     if self.PositionLong > 0:
                         self.Sell(self.O[-1], self.p_lots, '卖平')
-                    if mahurst > 0.6:
+                    if mahurst > 1 and mahurst < 1.25:
                         self.SellShort(self.O[-1], self.p_lots, '卖开')
 
-            if mahurst < 0.45 and self.PositionLong >0:
+            if ((mahurst < 0.5 and mahurst > 0.3) or mahurst > 1.25 )and self.PositionLong >0:
                 self.Sell(self.O[-1], self.p_lots, '卖平')
             
-            if mahurst < 0.45 and self.PositionShort >0:
+            if ((mahurst < 0.5 and mahurst > 0.3) or mahurst > 1.25) and self.PositionShort >0:
                 self.BuyToCover(self.O[-1], self.p_lots, '买平')
             
